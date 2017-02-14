5ce24dbed52acf83277c0efc44f00e75f6192b1e
Tejas Sharma <sharmatejas0@gmail.com>
2017-02-14 09:37:26 -0500

    Moving most of the functionality to the python script(s)

--- todo.py
+++ todo.py
@@ -1,78 +1,16 @@
-import argparse
-import os
 import datetime
-import re
+import os
+import todo_helper
+import subprocess
 
-class Item:
-    def __init__(self, text, completed):
-        self._text = text
-        self._completed = completed
+now = datetime.datetime.now()
+today = '{:04}{:02}{:02}.txt'.format(now.year, now.month, now.day)
+yestDate = datetime.date.today()-datetime.timedelta(1)
+yest = '{:04}{:02}{:02}.txt'.format(yestDate.year, yestDate.month, yestDate.day)
 
-class Section:
-    def __init__(self, name, items):
-        self._name = name
-        self._items = items
-        
-def do_copy(yest, today):
-    if not os.path.exists(yest):
-        file(today, 'w').close()
-    else:
-        with open(yest, 'r') as f:
-            contents = list(f)
-        if contents != None:
-            finalContents = [line for line in contents if not line.startswith('x ')]
-        with open(today, 'w') as f:
-            f.write('\n'.join([n.rstrip() for n in finalContents]))
+if os.path.exists(today):
+  todo_helper.do_cleanup(today)
+else:
+  todo_helper.do_copy(yest, today)
 
-def parse_file(fileToParse):
-    sections = []
-    if os.path.exists(fileToParse):
-        with open(fileToParse) as f:
-            contents = list(f)
-    items = []
-    section = ''
-    for content in contents:
-        if content.startswith('##'):
-            if (section != ''):
-                sections.append(Section(section, items))
-            items = []
-            matches = re.compile('^##(.*)##$').match(content)
-            section = matches.group(1)
-        else:
-            items.append(Item(content[1:] if content.startswith('x ') else content, content.startswith('x ')))
-    if section != '':
-        sections.append(Section(section, items))
-    return sections;
-
-def do_cleanup(fileToParse):
-    sections = parse_file(fileToParse)
-    with open(fileToParse, 'w') as f:
-        for section in sections:
-            print('##{}##'.format(section._name), file=f)
-            for unfinished in [item._text.rstrip() for item in section._items if not item._completed]:
-                print(unfinished, file=f)
-            for finished in [item._text.rstrip() for item in section._items if item._completed]:
-                print('x {}'.format(finished), file=f)
-            print('', file=f)
-
-
-parser = argparse.ArgumentParser(description='Utility for managing todo lists')
-
-rootGroup = parser.add_mutually_exclusive_group()
-
-copyGroup = rootGroup.add_argument_group('copy')
-copyGroup.add_argument('--yest', help='Filename for yesterday\'s todo')
-copyGroup.add_argument('--today', help='Filename for today\'s todo')
-copyGroup.add_argument('--copy', action='store_true')
-
-cleanGroup = rootGroup.add_argument_group('clean')
-cleanGroup.add_argument('--clean', action='store_true')
-cleanGroup.add_argument('--file', help='Filename')
-
-args = parser.parse_args()
-
-if args.copy:
-    do_copy(args.yest, args.today)
-elif args.clean:
-    do_cleanup(args.file)
 
---
+++ todo_helper.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+import datetime
+import re
+
+class Item:
+    def __init__(self, text, completed):
+        self._text = text
+        self._completed = completed
+
+class Section:
+    def __init__(self, name, items):
+        self._name = name
+        self._items = items
+        
+def do_copy(yest, today):
+    if not os.path.exists(yest):
+        file(today, 'w').close()
+    else:
+        with open(yest, 'r') as f:
+            contents = list(f)
+        if contents != None:
+            finalContents = [line for line in contents if not line.startswith('x ')]
+        with open(today, 'w') as f:
+            f.write('\n'.join([n.rstrip() for n in finalContents]))
+
+def parse_file(fileToParse):
+    sections = []
+    if os.path.exists(fileToParse):
+        with open(fileToParse) as f:
+            contents = list(f)
+    items = []
+    section = ''
+    for content in contents:
+        if content.startswith('##'):
+            if (section != ''):
+                sections.append(Section(section, items))
+            items = []
+            matches = re.compile('^##(.*)##$').match(content)
+            section = matches.group(1)
+        else:
+            items.append(Item(content[1:] if content.startswith('x ') else content, content.startswith('x ')))
+    if section != '':
+        sections.append(Section(section, items))
+    return sections;
+
+def do_cleanup(fileToParse):
+    sections = parse_file(fileToParse)
+    with open(fileToParse, 'w') as f:
+        for section in sections:
+            print('##{}##'.format(section._name), file=f)
+            for unfinished in [item._text.rstrip() for item in section._items if not item._completed]:
+                print(unfinished, file=f)
+            for finished in [item._text.rstrip() for item in section._items if item._completed]:
+                print('x {}'.format(finished), file=f)
+            print('', file=f)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Utility for managing todo lists')
+
+  rootGroup = parser.add_mutually_exclusive_group()
+
+  copyGroup = rootGroup.add_argument_group('copy')
+  copyGroup.add_argument('--yest', help='Filename for yesterday\'s todo')
+  copyGroup.add_argument('--today', help='Filename for today\'s todo')
+  copyGroup.add_argument('--copy', action='store_true')
+
+  cleanGroup = rootGroup.add_argument_group('clean')
+  cleanGroup.add_argument('--clean', action='store_true')
+  cleanGroup.add_argument('--file', help='Filename')
+
+  args = parser.parse_args()
+
+  if args.copy:
+      do_copy(args.yest, args.today)
+  elif args.clean:
+      do_cleanup(args.file)
+
